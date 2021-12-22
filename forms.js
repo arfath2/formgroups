47f0bef97362ea2d75f9456d25459990bf798803
arfath2 <arfath23mohammed@gmail.com>
2021-12-22 20:50:02 +0530

    adding li

--- forms.js
+++ forms.js
@@ -1,19 +1,28 @@
 const myForm = document.querySelector('#my-form');
+const userList = document.querySelector('#users');
 
 
 myForm.addEventListener('submit', onSubmit);
 
 function onSubmit(e) {
     e.preventDefault();
-    var name = document.getElementById("name").value;
-    let nameString = JSON.stringify(name);
-    localStorage.setItem('name', nameString );
-    var email = document.getElementById("email").value;
-    let emailString = JSON.stringify(email);
-    localStorage.setItem('email', emailString );
+    const name = document.getElementById("name").value;
+    const emailId = document.getElementById("email").value;
+    if (emailId.length > 0 && name.length > 0) {
+      const object = {
+        name: name,
+        emailId: emailId 
+      };
+      localStorage.setItem("userDetails", JSON.stringify(object));
 
-    let object = JSON.parse(localStorage.getItem(email))
-    console.log(object)
-    name.value = '';
-    email.value = '';
+      const li = document.createElement('li');
+
+      li.appendChild(document.createTextNode(`${name}: ${emailId}`));
+  
+      userList.appendChild(li);
+    } 
+
 }
+
+
+  
